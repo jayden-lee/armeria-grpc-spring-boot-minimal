c6de44bfe0c9bc72d664079f727b8585e815885b
이광기 <gwanggi.lee@gmail.com>
2021-02-13 22:10:51 +0900

    Add micrometer

--- src/main/java/com/jayden/example/config/ArmeriaConfig.java
+++ src/main/java/com/jayden/example/config/ArmeriaConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import com.jayden.example.grpc.GreeterServiceImpl;
+import com.linecorp.armeria.common.grpc.GrpcMeterIdPrefixFunction;
 import com.linecorp.armeria.common.grpc.GrpcSerializationFormats;
 import com.linecorp.armeria.server.TransientHttpService;
 import com.linecorp.armeria.server.docs.DocService;
@@ -11,11 +12,19 @@ import com.linecorp.armeria.server.grpc.GrpcService;
 import com.linecorp.armeria.server.healthcheck.HealthCheckService;
 import com.linecorp.armeria.server.logging.AccessLogWriter;
 import com.linecorp.armeria.server.logging.LoggingService;
+import com.linecorp.armeria.server.metric.MetricCollectingService;
 import com.linecorp.armeria.spring.ArmeriaServerConfigurator;
 import io.grpc.protobuf.services.ProtoReflectionService;
+import io.micrometer.core.instrument.MeterRegistry;
 
 @Configuration
 public class ArmeriaConfig {
+
+  private MeterRegistry meterRegistry;
+
+  public ArmeriaConfig(MeterRegistry meterRegistry) {
+    this.meterRegistry = meterRegistry;
+  }
 
   @Bean
   public ArmeriaServerConfigurator armeriaServerConfigurator() {
@@ -24,6 +33,8 @@ public class ArmeriaConfig {
       builder.serviceUnder("/docs", new DocService());
 
       builder.decorator(LoggingService.newDecorator());
+      builder.decorator(MetricCollectingService.newDecorator(GrpcMeterIdPrefixFunction.of("grpc.service")));
+
       builder.accessLogWriter(AccessLogWriter.combined(), false);
 
       builder.service(
@@ -34,6 +45,8 @@ public class ArmeriaConfig {
               .enableUnframedRequests(true)
               .useBlockingTaskExecutor(true)
               .build());
+
+      builder.meterRegistry(meterRegistry);
     };
   }
 }
